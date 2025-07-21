eee5ff9c8c5af6247000205fbf9627facaa8a530
Surya0265 <suryaprakashb265@gmail.com>
2025-07-21 21:13:10 +0530

    done ok

--- hooks/useFrameworkReady.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-
-export function useFrameworkReady() {
-  useEffect(() => {
-    // Framework initialization logic
-    console.log('Framework ready');
-  }, []);
-}
